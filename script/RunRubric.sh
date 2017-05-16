b8cb76dfa58640d6321eaab2a4d1760a9ba177d0
scgordon <scgordon@hdfgroup.org>
2017-05-16 08:37:13 -0600

    getItIn
    directories, metadata, data, scripts

---
+++ script/RunRubric.sh
@@ -0,0 +1,15 @@
+#This is a batch script that runs the needed instances of Rubric.sh
+#Rubric runs the xsl WriteDialectXSL to create a xsl rubric used to assess the collections records 
+#It requires one argument: the dialect code of the dialect needed for analysis.
+
+./Rubric.sh DIF
+./Rubric.sh CSDGM
+./Rubric.sh BDP
+./Rubric.sh EML
+./Rubric.sh Mercury
+./Rubric.sh Dryad
+./Rubric.sh Onedcx
+./Rubric.sh RDA-CISL
+./Rubric.sh ISO
+./Rubric.sh DCITE
+./Rubric.sh MODS
