e934d2efe43ada886f11e309ddd689a74cd6ca7e
scgordon <scgordon@hdfgroup.org>
2017-05-16 10:22:44 -0600

    realigningScripts
    should be able to make these work from the notebook!

--- script/Data.sh
+++ script/Data.sh
@@ -2,18 +2,16 @@
 #a RAD.xlsx functions on. It requires three arguments. The first is the organization the second 
 #is the dialect, the third is the collection. GetData.sh is the batch script for Data.sh
 cd ../
-ConceptMining=$(pwd)
-cd ../CrosswalksWorkflow
-CrosswalkWorkflow=$(pwd)
+Mile2=$(pwd)
 cd ../Crosswalks 
 CrosswalkHome=$(pwd) 
 
     java net.sf.saxon.Transform \
-    -s:$CrosswalkWorkflow/collections/dummy.xml \
-    -xsl:$CrosswalkHome/dialectRubrics/rubric$2test3.xsl \
-    -o:$ConceptMining/data/$1/$3_$2_data.csv \
+    -s:$Mile2/collection/dummy.xml \
+    -xsl:$CrosswalkHome/dialectRubrics/rubric$2.xsl \
+    -o:$Mile2/data/$1/$3_$2_data.csv \
     fileNamePattern=*.xml \
-    recordSetPath=$CrosswalkWorkflow/collections/$1/$3/$2/xml \
+    recordSetPath=$Mile2/collection/$1/$3/$2/xml \
     dialectName=$2 \
     delimiter=',' \
    
--- script/RubricCleaner.sh
+++ script/RubricCleaner.sh
@@ -1,11 +1,13 @@
 #sed commands to clean namespace references if neccessary. Most NASA cases do not require this
 #script to be run.
-sed -i -e 's|/csdgm:|/|g' /Users/scgordon/Crosswalks/dialectRubrics/rubricCSDGM.xsl 
-sed -i -e 's|/bdp:|/|g' /Users/scgordon/Crosswalks/dialectRubrics/rubricBDP.xsl 
-sed -i -e 's|/mercury:|/|g' /Users/scgordon/Crosswalks/dialectRubrics/rubricMercury.xsl
-sed -i -e 's|/eml:|/|g' /Users/scgordon/Crosswalks/dialectRubrics/rubricEML.xsl &&
-sed -i -e 's|"/eml/|"/eml:eml/|g' /Users/scgordon/Crosswalks/dialectRubrics/rubricEML.xsl
-sed -i -e 's|/rda:|/|g' /Users/scgordon/Crosswalks/dialectRubrics/rubricRDA-CISL.xsl
-sed -i -e 's|/dcite:|/|g' /Users/scgordon/Crosswalks/dialectRubrics/rubricDCITE.xsl
-sed -i -e 's|/mods:|/|g' /Users/scgordon/Crosswalks/dialectRubrics/rubricMODS.xsl 
-sed -i -e 's|/mercury:|/|g' /Users/scgordon/Crosswalks/dialectRubrics/rubricMercury.xsl 
+cd ../../Crosswalks
+CrosswalkHome=$(pwd)
+sed -i -e 's|/csdgm:|/|g' $CrosswalkHome/dialectRubrics/rubricCSDGM.xsl 
+sed -i -e 's|/bdp:|/|g' $CrosswalkHome/dialectRubrics/rubricBDP.xsl 
+sed -i -e 's|/mercury:|/|g' $CrosswalkHome/dialectRubrics/rubricMercury.xsl
+sed -i -e 's|/eml:|/|g' $CrosswalkHome/dialectRubrics/rubricEML.xsl &&
+sed -i -e 's|"/eml/|"/eml:eml/|g' $CrosswalkHome/dialectRubrics/rubricEML.xsl
+sed -i -e 's|/rda:|/|g' $CrosswalkHome/dialectRubrics/rubricRDA-CISL.xsl
+sed -i -e 's|/dcite:|/|g' $CrosswalkHome/dialectRubrics/rubricDCITE.xsl
+sed -i -e 's|/mods:|/|g' $CrosswalkHome/dialectRubrics/rubricMODS.xsl 
+sed -i -e 's|/mercury:|/|g' $CrosswalkHome/dialectRubrics/rubricMercury.xsl 
