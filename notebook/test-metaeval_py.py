d457e5cd904dcb030a0be8eec882d7f5d7bc0eeb
scgordon <scgordon@hdfgroup.org>
2017-10-19 19:01:07 -0600

    NotebookImprovements
    markdown mainly

--- notebook/test-metaeval_py.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-url = 'http://metadig.nceas.ucsb.edu/metadata/evaluator'
-files = {'zipxml': open('../upload/metadata.zip', 'rb')}
-r = requests.post(url, files=files)
-r.raise_for_status()
